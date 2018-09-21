d589a117fe5de91f955728d3dfe52b90b7b921bf
Jonathan Perret <jonathan@ut7.fr>
2018-09-21 15:27:50 +0200

    Basic Express+Webpack+Docker setup

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:10.11.0-alpine as build
+
+COPY package.json package-lock.json /app/
+
+WORKDIR /app
+
+RUN npm ci
+
+COPY webpack.config.js /app/
+COPY src/client /app/src/client
+RUN npm run build
+
+FROM node:10.11.0-alpine
+
+COPY package.json package-lock.json /app/
+
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+RUN npm ci
+
+COPY . /app
+COPY --from=build /app/public/ /app/public/
+
+EXPOSE 3000
+
+CMD ["node", "bin/www"]
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src/client')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [
+            [
+              'env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
+
+        test: /\.js$/
+      }
+    ]
+  },
+
+  entry: {
+    editor: './src/client/editor.js',
+    player: './src/client/player.js'
+  },
+
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'public')
+  },
+
+  mode: process.env.NODE_ENV || 'development',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      hash: true,
+      chunks: ['player'],
+      template: path.resolve(__dirname, 'src/client/player.html')
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'editor.html',
+      hash: true,
+      chunks: ['editor'],
+      template: path.resolve(__dirname, 'src/client/editor.html')
+    }),
+  ],
+};
