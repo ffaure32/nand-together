dc5f395945583aebf53da9e388d93cc10ef1c646
Jonathan Perret <jonathan@ut7.fr>
2018-09-21 18:16:16 +0200

    Basic player interface

--- src/client/player.js
+++ src/client/player.js
@@ -1,1 +1,26 @@
-alert('todo!');
+import p5 from 'p5';
+import './css/common.css';
+
+let state = false;
+
+new p5(function (p) {
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = function () {
+    p.background(130, 130, 240);
+
+    p.fill(state ? 255 : 0);
+
+    p.rect(.25 * p.width, 0.25 * p.height, .5 * p.width, .5 * p.height);
+  };
+
+  p.mousePressed = function () {
+    state = !state;
+  }
+});
