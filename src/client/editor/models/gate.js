3b761ae88a7f20e638ad25ebaac1cfbae51dceaf
Jonathan Perret <jonathan@ut7.fr>
2018-09-23 17:42:49 +0200

    Display gate id and playerId outside box

--- src/client/editor/models/gate.js
+++ src/client/editor/models/gate.js
@@ -16,12 +16,12 @@ export default class Gate {
     p.fill(this.state ? 255 : 0);
     p.rect(0, 0, this.size.x, this.size.y);
 
-    p.textAlign(p.CENTER, p.TOP);
+    p.textAlign(p.CENTER, p.BOTTOM);
     p.textSize(10);
     p.fill(0);
-    p.text(this.id, 0.5 * this.size.x, 2);
-    p.textAlign(p.CENTER, p.BOTTOM);
-    p.text(this.playerId || "", 0.5 * this.size.x, this.size.y - 2);
+    p.text(this.id, 0.5 * this.size.x, -2);
+    p.textAlign(p.CENTER, p.TOP);
+    p.text(this.playerId || "", 0.5 * this.size.x, this.size.y + 2);
     p.pop();
   }
 
